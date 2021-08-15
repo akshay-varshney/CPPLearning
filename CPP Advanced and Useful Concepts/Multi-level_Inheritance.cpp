d7d6faad2bc44354ee893bfb0365be596f524d35
sud <akshayvarshney.001@gmail.com>
2021-08-15 13:34:35 +0530

    Multi Level Inheritance

---
+++ CPP Advanced and Useful Concepts/Multi-level_Inheritance.cpp
@@ -0,0 +1,76 @@
+// This example will show the usage of the multi level inheritance
+
+#include <iostream>
+using namespace std;
+
+// Syntax for inheriting in multiple inheritance
+// class DerivedC: visibility-mode base1, visibility-mode base2
+// {
+//      Class body of class "DerivedC"
+// };
+
+class Base1{
+protected:
+    int base1int;
+
+public:
+    void set_base1int(int a)
+    {
+        base1int = a;
+    }
+};
+
+class Base2{
+protected:
+    int base2int;
+
+public:
+    void set_base2int(int a)
+    {
+        base2int = a;
+    }
+};
+
+class Base3{
+protected:
+    int base3int;
+
+public:
+    void set_base3int(int a)
+    {
+        base3int = a;
+    }
+};
+
+// Multi level inheritance derieved from 3 base classes.
+class Derived : public Base1, public Base2, public Base3
+{
+    public:
+        void display(){
+            cout << "The value of Base1 is " << base1int<<endl;
+            cout << "The value of Base2 is " << base2int<<endl;
+            cout << "The value of Base3 is " << base3int<<endl;
+            cout << "The sum of these values is " << base1int + base2int + base3int << endl;
+        }
+};
+/*
+The inherited derived class will look something like this:
+Data members:
+    base1int --> protected
+    base2int --> protected
+
+Member functions:
+    set_base1int() --> public
+    set_base2int() --> public
+    set_show() --> public
+*/
+int main()
+{
+    Derived var;
+    var.set_base1int(2);
+    var.set_base2int(5);
+    var.set_base3int(9);
+    var.display();
+    
+    return 0;
+}
